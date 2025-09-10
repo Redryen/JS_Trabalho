d7f3485d4dac98ffc22e4340a317d65b01fa0e94
Redryen <redryen.chula.ifpr.2023@gmail.com>
2025-09-10 16:13:03 -0300

    mudanca

--- js.js
+++ js.js
@@ -1,19 +1,25 @@
-// Gera o número aleatório de 1 a 100 ao carregar a página
-const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+var numero, palpite, mensagem
+ numero = Math.floor(Math.random() * 100) + 1;
 
 function verificarPalpite() {
-  const palpite = parseInt(document.getElementById("palpite").value);
-  const mensagem = document.getElementById("mensagem");
+   palpite = parseInt(document.getElementById("palpite").value);
+   mensagem = document.getElementById("mensagem");
 
 
-  if (palpite < numeroSecreto) {
+  if (palpite < numero) {
     mensagem.textContent = "Muito baixo!";
     mensagem.style.color = "blue";
-  } else if (palpite > numeroSecreto) {
+  } 
+  
+  else if (palpite > numero) {
     mensagem.textContent = "Muito alto!";
     mensagem.style.color = "red";
-  } else {
-    mensagem.textContent = "Você acertou!";
+  } 
+  
+  else {
+    mensagem.textContent = "Você acertou! O número aleatório era: " + numero ;
     mensagem.style.color = "green";
   }
+
+
 }
